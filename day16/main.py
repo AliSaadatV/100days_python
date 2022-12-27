41b1ea24397b50600a5acb09bf92bb99cecdd295
AliSaadatV <ali.saadat@epfl.ch>
2022-12-27 11:15:13 +0100

    day16 added

---
+++ day16/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+turn_off = False
+while not turn_off:
+    user_choice = input(f"What would you like? {menu.get_items()}: ")
+    if user_choice == "off":
+        turn_off = "True"
+    elif user_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+        

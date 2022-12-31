844b9f0871a668d433d61329a67c0a9a14d43d0b
AliSaadatV <ali.saadat@epfl.ch>
2022-12-31 14:27:51 +0100

    day20 added

---
+++ day20/snake.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+START_X_COR = [0, -20, -40]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    
+    def __init__(self):
+        self.segments = []
+        for i in range(3):
+            self.segments.append(Turtle(shape="square"))
+            self.segments[i].color("white")
+            self.segments[i].penup()
+            self.segments[i].setx(START_X_COR[i])
+        self.head = self.segments[0]
+
+    def move(self):
+        for i in range(len(self.segments)-1, 0, -1):
+            self.segments[i].setpos(self.segments[i-1].pos())
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    
+
